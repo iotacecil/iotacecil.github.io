ffb423bfc8ae80bc3063984c3d6353c5df4e8cbc
iotacecil <iotacecil@gmail.com>
2019-02-04 13:53:31 +0800

    windows自动提交定时任务

--- pushBlog/_pushBlog.bat
+++ pushBlog/_pushBlog.bat
@@ -3,10 +3,10 @@ call echo %cd%
 call echo %cd%
 
 call coscmd upload -rs %cd%\static\images /image
-call hexo g -d
 call git fetch
 call git merge
 call git add .
 call git commit -m"windows自动提交定时任务"
 call git push
+call hexo g -d
 
